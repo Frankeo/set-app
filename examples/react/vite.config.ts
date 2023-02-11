03fdc713c2e70f8ac56eb83ca6a578b6d5d6c6eb
Francisco Leonel Moreno Eraso <f.moreno@aligntech.com>
2023-02-11 06:37:52 +0100

    build(turbo): add turbo to the monorepo
    Create task for pre-commit ci and cd for being used during github actions execution

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ react() ],
   root: "src",
   test: {
     environment: "happy-dom",

06f6b419d5a64e88b0a8a8cbe20653c3ff960fe0
Francisco Moreno <ingfrancisco.moreno1@gmail.com>
2023-02-23 22:39:44 +0100

    fix(@set-app/cli): fix readme generator invocation (#40)

--- packages/cli/src/create-readme.ts
+++ packages/cli/src/create-readme.ts
@@ -2,6 +2,6 @@ import { $ } from "zx";
 import { getSuccessMessage } from "./interface/messages.js";
 
 export const createReadme = async (projectPath: string) => {
-  await $`cd ${projectPath} ; npx readme-md-generator -y ;`;
+  await $`cd ${projectPath} ; npx --yes readme-md-generator -y ;`;
   getSuccessMessage("created", "README.md");
 };
